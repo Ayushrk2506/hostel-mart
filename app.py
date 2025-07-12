e1a6e0c247b62a3442857d19ad5daf2102f00807
Ayushrk2506 <ayushr.cs24@bmsce.ac.in>
2025-07-13 00:35:10 +0530

    Fix: updated DB path for external database

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ UPI_ID = "9019531019@ybl"
 
 # --- DB Connection ---
 def get_db_connection():
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    db_path = os.path.join(base_dir, 'database.db')
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    db_path = os.path.join(base_dir, '..', 'database.db')
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
